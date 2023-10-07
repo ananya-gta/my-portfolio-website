daa32f0d3351de6a762391383b9b9a32a060f5e6
Ananya Gupta <anannya.gta@gmail.com>
2023-10-07 16:10:57 +0530

    Added Socials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assests/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,13 @@ const Header = () => {
         <h1>Ananya Gupta</h1>
         <h5 className="text-light">Software Developer</h5>
         <CTA />
+
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="Ananya's Photo" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href="https://www.linkedin.com/in/ananya-gupta-1902/" target='_blank'><BsLinkedin /></a>
+        <a href="https://github.com/ananya-gta" target='_blank'></a>
+        <a href="https://twitter.com/ananya_192" target='_blank'></a>
+        <a href="https://discord.com/users/864127704231968789" target='_blank'></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
