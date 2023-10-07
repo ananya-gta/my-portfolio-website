b8dbee75455eca432e48a29d2fd7bb6fc5ea647c
Ananya Gupta <anannya.gta@gmail.com>
2023-10-07 17:41:29 +0530

    Header of the portfolio is finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assests/me.png'
+import ME from '../../assests/me1.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Ananya Gupta</h1>
-        <h5 className="text-light">Software Developer</h5>
+        <h5 className="text-light">Front-End Developer</h5>
         <CTA />
 
         <HeaderSocials />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
+import {FaLinkedin, FaGithub, FaTwitter, FaDiscord} from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/ananya-gupta-1902/" target='_blank'><BsLinkedin /></a>
-        <a href="https://github.com/ananya-gta" target='_blank'></a>
-        <a href="https://twitter.com/ananya_192" target='_blank'></a>
-        <a href="https://discord.com/users/864127704231968789" target='_blank'></a>
+        <a href="https://www.linkedin.com/in/ananya-gupta-1902/" target='_blank'><FaLinkedin /></a>
+        <a href="https://github.com/ananya-gta" target='_blank'><FaGithub /></a>
+        <a href="https://twitter.com/ananya_192" target='_blank'><FaTwitter/></a>
+        <a href="https://discord.com/users/864127704231968789" target='_blank'><FaDiscord/></a>
     </div>
   )
 }
