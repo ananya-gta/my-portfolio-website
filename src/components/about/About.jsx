dab70abc92584a970cb4a9c95a0f29ccd0ee79fb
Ananya Gupta <anannya.gta@gmail.com>
2024-02-01 17:54:35 +0530

    Done with About section, left with css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assests/aboutme.png'
-import {GrUserWorker} from 'react-icons/gr'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 export const About = () => {
   return (
@@ -18,11 +20,32 @@ export const About = () => {
 
     <div className="about__content">
       <div className="about__cards">
+
         <article className="about__card">
+          <FaAward className='about__icon' />
           <h5>Experience</h5>
           <small>10 months of work experience</small>
         </article>
+
+        <article className="about__card">
+          <FiUsers className='about__icon' />
+          <h5>Clients</h5>
+          <small>200+ Worldwide</small>
+        </article>
+
+        <article className="about__card">
+          <VscFolderLibrary className='about__icon' />
+          <h5>Projects</h5>
+          <small>10+ Completed Projects</small>
+        </article>
+
       </div>
+
+      <p>
+        Hi! I am a web developer and designer. My name is Ananya Gupta. 
+      </p>
+
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
    </div>
    </section>
