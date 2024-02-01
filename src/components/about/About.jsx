5c2467150ff927980a013e7cc8458b6dee6e30d2
Ananya Gupta <anannya.gta@gmail.com>
2024-02-01 22:06:03 +0530

     Refactoring names and started experience section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,55 +1,64 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assests/aboutme.png'
-import {FaAward} from 'react-icons/fa'
+import React from "react";
+import "./about.css";
+import ME from "../../assests/aboutme.png";
+import { FaAward } from "react-icons/fa";
 import { FaPenNib } from "react-icons/fa";
-import {VscFolderLibrary} from 'react-icons/vsc'
+import { VscFolderLibrary } from "react-icons/vsc";
 
 export const About = () => {
   return (
-   <section id="about">
-    <h5>Get to Know</h5>
-    <h2>About Me</h2>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
 
-   <div className="container about__container">
-    <div className="about__me">
-      <div className="about__me-image">
-        <img src={ME} alt="About me Image" />
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About me Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <a href="#experience">
+              <article className="about__card">
+                <FaAward className="about__icon" />
+                <h5>Experience</h5>
+              </article>
+            </a>
+
+            <a href="#services">
+              <article className="about__card">
+                <FaPenNib className="about__icon" />
+                <h5>Blogs</h5>
+              </article>
+            </a>
+
+            <a href="#projects">
+              <article className="about__card">
+                <VscFolderLibrary className="about__icon" />
+                <h5>Projects</h5>
+              </article>
+            </a>
+          </div>
+
+          <p id="about-me">
+            Hello! I'm Ananya, based in Gurugram, India. With a love for clean
+            code and captivating design, I am learning how to craft beautiful,
+            aesthetic and functional user interfaces. I also enjoy sharing my
+            knowledge and insights through writing engaging content on
+            technology and development. When I'm not busy coding, you can find
+            me exploring new cafes, capturing moments through my camera lens, or
+            lost in the pages of a good book.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
       </div>
-    </div>
+    </section>
+  );
+};
 
-    <div className="about__content">
-      <div className="about__cards">
-
-        <article className="about__card">
-          <FaAward className='about__icon' />
-          <h5>Experience</h5>
-          <small>10 months of work experience</small>
-        </article>
-
-        <article className="about__card">
-          <FaPenNib className='about__icon' />
-          <h5>Blogs</h5>
-          <small>My Blogs on Medium and Hashnode</small>
-        </article>
-
-        <article className="about__card">
-          <VscFolderLibrary className='about__icon' />
-          <h5>Projects</h5>
-          <small>10+ Completed Projects</small>
-        </article>
-
-      </div>
-
-      <p>
-        Hi! I am a web developer and designer. My name is Ananya Gupta. 
-      </p>
-
-      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-    </div>
-   </div>
-   </section>
-  )
-}
-
-export default About
+export default About;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBookshelf /></a>
-      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFolderOpen /></a>
+      <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><AiOutlineFolderOpen /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TiContacts /></a>
     </nav>
   )
