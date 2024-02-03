e61be82c1099fb8616f683a51997ea9eda943e36
Ananya Gupta <anannya.gta@gmail.com>
2024-02-03 17:51:07 +0530

    Finished with Projects.css and started with contact section, writing the jsx part

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineMail } from "react-icons/md";
 
 export const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+          <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>ananyagupta.works@gmail.com</h5>
+            <a href="mailto:ananyagupta.works@gmail.com">Send a message</a>
+          </article>
+        </div>
+      </div>
+      </section>
   )
 }
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,6 +4,36 @@ import IMG1 from "../../assests/sociobuzz.png";
 import IMG2 from "../../assests/imaginai.png";
 import IMG3 from "../../assests/deja-brew.jpg";
 
+// array of projects
+// now whenever you want to add a new project just add json in the data array
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "SocioBuzz",
+    desc: "This is a responsive social media website with theme customization. It was made to practice HTML, CSS & JavaScript. It has an interactive and visually appealing UI. Know more about SocioBuzz on GitHub.",
+    github: "https://github.com/ananya-gta/sociobuzz",
+    deployed_url: "https://sociobuzz.netlify.app/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "ImaginAI",
+    desc: "ImaginAI is a user-friendly image generator inspired by DALL·E. The simple UI of ImaginAI is built using Flask & HTML/CSS/Javascript. The project is hosted and developed on the Replit platform.",
+    github: "https://github.com/ananya-gta/imaginai",
+    deployed_url: "https://imaginai.ananya-gta.repl.co/",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Deja Brew",
+    desc: "This is a user-friendly front end of a cafe website. It was made to have hands-on experience in the Django Framework. Also leveraged the Bootstrap framework for designing and styling the website.",
+    github: "https://github.com/ananya-gta/cafe-website",
+    deployed_url:
+      "https://github.com/ananya-gta/cafe-website/blob/main/README.md",
+  },
+];
+
 const Projects = () => {
   return (
     <section id="projects">
@@ -11,77 +41,30 @@ const Projects = () => {
       <h2>Projects</h2>
 
       <div className="projects__container container">
-        <article className="projects__item">
-          <div className="projects__item-image">
-            <img src={IMG1} alt="sociobuzz home page" />
-          </div>
-          <h3>SocioBuzz</h3>
-          <small>
-            This is a responsive social media website with theme customization.
-            It was made to practice HTML, CSS & JavaScript. It has an
-            interactive and visually appealing UI.
-          </small>
-          <a href="https://github.com/ananya-gta/sociobuzz" className="btn">
-            GitHub
-          </a>
-          <a
-            href="https://sociobuzz.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
-        </article>
-        {/* END OF PROJECT-SOCIOBUZZ  */}
-
-        <article className="projects__item">
-          <div className="projects__item-image">
-            <img src={IMG2} alt="ImaginAI home page" />
-          </div>
-          <h3>ImaginAI</h3>
-          <small>
-            This is a responsive social media website with theme customization.
-            It was made to practice HTML, CSS & JavaScript. It has an
-            interactive and visually appealing UI.
-          </small>
-          <a href="https://github.com/ananya-gta/imaginai" className="btn">
-            GitHub
-          </a>
-          <a
-            href="https://imaginai.ananya-gta.repl.co/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
-        </article>
-        {/* END OF PROJECT-ImaginAI  */}
-
-        <article className="projects__item">
-          <div className="projects__item-image">
-            <img src={IMG3} alt="Deja brew home page" />
-          </div>
-          <h3>Deja Brew</h3>
-          <small>
-            This is a responsive social media website with theme customization.
-            It was made to practice HTML, CSS & JavaScript. It has an
-            interactive and visually appealing UI.
-          </small>
-          <a href="https://github.com/ananya-gta/cafe-website" className="btn">
-            GitHub
-          </a>
-          <a
-            href="https://github.com/ananya-gta/cafe-website/blob/main/README.md"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
-        </article>
-        {/* END OF PROJECT-Deja-Brew  */}
+        {data.map(({ id, image, title, desc, github, deployed_url }) => {
+          return (
+            <article key={id} className="projects__item">
+              <div className="projects__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <h5>{desc}</h5>
+              <div className="projects__item-cta">
+                <a href={github} className="btn">
+                  GitHub
+                </a>
+                <a
+                  href={deployed_url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-primary"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
