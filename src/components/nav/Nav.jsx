853eff0eaed0fdd68e89b6a49f556a152ecbe1cc
Ananya Gupta <anannya.gta@gmail.com>
2024-02-27 13:20:46 +0530

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#kills' ? 'active' : ''}><BsBookshelf /></a>
+      <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><BsBookshelf /></a>
       <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><AiOutlineFolderOpen /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TiContacts /></a>
     </nav>
